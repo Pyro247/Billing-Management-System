1ae51019fc6e242e84c9ef71b802bf1113bdc719
Justine Dave <djustine247@gmail.com>
2021-08-27 19:20:09 +0800

    Added new features like: System ui, login, forgot password and added all the folders

---
+++ Javascript_files/popup.js
@@ -0,0 +1,13 @@
+let userID = document.getElementById('userID-popup');
+
+function studentPopup(){
+    userID.textContent = "STUDENT"
+}
+
+function registrarPopup(){
+    userID.textContent = "REGISTRAR"
+}
+
+function cashierPopup(){
+    userID.textContent = "CASHIER"
+}
