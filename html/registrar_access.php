1a43f2497a1ae05df148de780fb35ba14e50ff66
Mikee1301 <isla.michael.estrecho@gmail.com>
2021-10-28 18:47:45 +0800

    Archice Data Table Fixed

--- html/registrar_access.php
+++ html/registrar_access.php
@@ -2098,9 +2098,13 @@
           }
         });
       </script>
+      <!-- Archives Tab Script -->
       <script>
         $(document).ready(function () {
-          
+          $('#v-pills-archives-tab').click(function (e) { 
+            viewArchiveDataTable()
+            
+          });
           $("#searchArchive_btn").click(function(){
             $.ajax({
               type:'POST',
@@ -2115,11 +2119,49 @@
               }
             });
           });
-          
+          function viewArchiveDataTable(){
+              $.ajax({
+                type: "POST",
+                url: "../includes/registrarArchive.php",
+                dataType: "html",
+                success: function (data) {
+                  $('#registrarArchive').html(data);
+                }
+              });
+          }
+          function exportTableToExcel(tableID, filename = ''){
+            var downloadLink;
+            var dataType = 'application/vnd.ms-excel';
+            var tableSelect = document.getElementById(tableID);
+            var tableHTML = tableSelect.outerHTML.replace(/ /g, '%20');
+            
+            // Specify file name
+            filename = filename?filename+'.xls':'excel_data.xls';
+            
+            // Create download link element
+            downloadLink = document.createElement("a");
+            
+            document.body.appendChild(downloadLink);
+            
+            if(navigator.msSaveOrOpenBlob){
+                var blob = new Blob(['\ufeff', tableHTML], {
+                    type: dataType
+                });
+                navigator.msSaveOrOpenBlob( blob, filename);
+            }else{
+                // Create a link to the file
+                downloadLink.href = 'data:' + dataType + ', ' + tableHTML;
+            
+                // Setting the file name
+                downloadLink.download = filename;
+                
+                //triggering the function
+                downloadLink.click();
+            }
+          }
         });
-     
       </script>
-       <!-- Student Fees -->
+       <!-- Student Fees Tab-->
     <script>
       $(document).ready(function () {
         // Initial Data Table
@@ -2171,48 +2213,6 @@
         });
       }
     </script>
-      <script>
-          // function display(){
-          //     $.ajax({
-          //       type: "POST",
-          //       url: "../includes/registrarArchive.php",
-          //       dataType: "html",
-          //       success: function (data) {
-          //         $('#registrarArchive').html(data);
-          //       }
-          //     });
-          // }
-          function exportTableToExcel(tableID, filename = ''){
-            var downloadLink;
-            var dataType = 'application/vnd.ms-excel';
-            var tableSelect = document.getElementById(tableID);
-            var tableHTML = tableSelect.outerHTML.replace(/ /g, '%20');
-            
-            // Specify file name
-            filename = filename?filename+'.xls':'excel_data.xls';
-            
-            // Create download link element
-            downloadLink = document.createElement("a");
-            
-            document.body.appendChild(downloadLink);
-            
-            if(navigator.msSaveOrOpenBlob){
-                var blob = new Blob(['\ufeff', tableHTML], {
-                    type: dataType
-                });
-                navigator.msSaveOrOpenBlob( blob, filename);
-            }else{
-                // Create a link to the file
-                downloadLink.href = 'data:' + dataType + ', ' + tableHTML;
-            
-                // Setting the file name
-                downloadLink.download = filename;
-                
-                //triggering the function
-                downloadLink.click();
-            }
-        }
-      </script>
       <!-- Script For Manage User-Employee Admin Access -->
       <script>
         let allData = '%'
